61f9eca1e2b76a38d110ee51482482193fc14f44
Furuglyas Kristof <78furu@gmail.com>
2021-05-21 13:06:35 +0000

    finished image generation w evolution

--- models.py
+++ models.py
@@ -9,36 +9,23 @@ class Score(nn.Module):
         self.config = config
         nef = config.model.nef
         self.u_net = nn.Sequential(
-            # input is (nc) x 28 x 28
             nn.Conv2d(config.data.channels, nef, 4, stride=2, padding=1),
-            # nn.Softplus(),
             nn.GroupNorm(4, nef),
             nn.ELU(),
-            # state size. (nef) x 14 x 14
             nn.Conv2d(nef, nef * 2, 4, stride=2, padding=1),
             nn.GroupNorm(4, nef * 2),
-            # nn.Softplus(),
             nn.ELU(),
-            # state size. (nef*2) x 7 x 7
             nn.Conv2d(nef * 2, nef * 4, 5, stride=1, padding=0),
             nn.GroupNorm(4, nef * 4),
-            # nn.Softplus(),
             nn.ELU(),
-            # state size. (nef*4) x 3 x 3
             nn.ConvTranspose2d(nef * 4, nef * 2, 5, stride=1, padding=0),
             nn.GroupNorm(4, nef * 2),
-            # nn.Softplus(),
             nn.ELU(),
-            # state size. (nef*2) x 7 x 7
             nn.ConvTranspose2d(nef * 2, nef, 4, stride=2, padding=1),
             nn.GroupNorm(4, nef),
-            # nn.Softplus(),
             nn.ELU(),
-            # state size. (nef) x 14 x 14
             nn.ConvTranspose2d(nef, config.data.channels, 4, stride=2, padding=1),
-            # nn.Softplus()
             nn.ELU()
-            # state size. (nc) x 28 x 28
         )
         self.fc = nn.Sequential(
             nn.Linear(config.data.channels * config.data.image_size * config.data.image_size, 1024),
